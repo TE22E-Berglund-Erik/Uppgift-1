dc190f447db5e64d6813b72bf9c40e4bcce88012
TE22E-Erik-Berglund <erik.berglund2@elev.ga.ntig.se>
2023-09-06 10:37:19 +0200

    Lagt till kod för heltals operationer

---
+++ src/App.java
@@ -0,0 +1,20 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+       int tal1 = 2;
+       int tal2 = 30;
+       
+       int summa = tal1+tal2;
+       int differens = tal2-tal1;
+
+        System.out.println("summan av tal + tal 2 är "+summa);
+        System.out.println("differensen av tal2 - tal1 är "+differens);
+        
+        int produkt=tal1*tal2;
+        int kvot = tal1/tal2;
+
+        System.out.println("produkten av tal1*tal2 är "+produkt);
+        System.out.println("kvoten av tal1/tal2 är "+kvot);
+
+        
+    }
+}
